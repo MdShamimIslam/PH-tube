21bc76cfe3f3a7243bfbf5adad33e9df3cac2992
MdShamimIslam <shamim401897@gmail.com>
2023-10-14 22:21:39 +0600

    show all category card

--- js/tube.js
+++ js/tube.js
@@ -6,17 +6,48 @@ const loadCategories = async () =>{
 }
 
 const showAllCategories = (categories)=>{
-    console.log(categories);
     const tabContainer = document.getElementById('tab-container');
     categories.forEach(category => {
-        console.log(category);
         const tabDiv = document.createElement('div');
         tabDiv.classList.add('tabs');
         tabDiv.innerHTML = `
-        <a class="tab text-lg text-black">${category.category}</a>
+        <a onClick="handleTabClick('${category.category_id}')" class="tab text-lg text-black">${category.category}</a>
         `;
         tabContainer.appendChild(tabDiv);
     })
 }
+
+const handleTabClick = async (categoryId) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await res.json();
+    const singleCategory = data.data;
+    console.log(singleCategory);
+    const cardContainer = document.getElementById('card-container');
+    singleCategory.forEach(perCategory => {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList = `card mt-8 bg-base-300 shadow-xl `
+    cardDiv.innerHTML = `
+
+        <figure><img src="${singleCategory.thumbnail
+        }" alt="" /></figure>
+        <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+        </div>
+        </div>
+
+    `;
+    cardContainer.appendChild(cardDiv);
+    })
+
+    
+
+    
+}
+
+
+
 
 loadCategories();
